d79fa54472fac7f1e39c68f7c164c4ab415c227c
Travis Tull <travis.tull.93@gmail.com>
2023-01-12 21:13:37 -0600

    End Of Section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Header } from 'semantic-ui-react';
+import List from 'semantic-ui-react/dist/commonjs/elements/List';
+import ListItem from 'semantic-ui-react/dist/commonjs/elements/List/ListItem';
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+    .then(response => {
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key = {activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
+
+    </div>
+  );
+}
+
+export default App;
