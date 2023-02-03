06b100edf51bb703c50d7c27006e4a92b3144f31
Travis Tull <travis.tull.93@gmail.com>
2023-02-02 21:20:38 -0600

    End Of Section 14

---
+++ Domain/ActivityAttendee.cs
@@ -0,0 +1,17 @@
+namespace Domain
+{
+    public class ActivityAttendee
+    {
+        public string AppUserID { get; set; }
+
+        public AppUser AppUser { get; set; }
+
+        public Guid ActivityId { get; set; }
+
+        public Activity Activity { get; set; }
+
+        public bool IsHost { get; set; }
+
+
+    }
+}
